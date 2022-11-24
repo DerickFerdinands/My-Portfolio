1d85d42884a96501ea641d06e1174f17f4d0401c
DerickFerdinands <derickandrewferdinands@gmail.com>
2022-11-24 12:58:24 +0530

    ContentU

--- assignments/JS/Game/Ludo/model/user.js
+++ assignments/JS/Game/Ludo/model/user.js
@@ -1,13 +1,16 @@
 let users = [
-    new User("user1",NavPath_1),
-    new User("user2",NavPath_2),
-    new User("user3",NavPath_3),
-    new User("user4",NavPath_4)];
+    new User("user1", NavPath_1, $('#cr1'), $('#cr2'), $('#cr3'), $('#cr4')),
+    new User("user2", NavPath_2, $('#cg1'), $('#cg2'), $('#cg3'), $('#cg4')),
+    new User("user3", NavPath_3, $('#cy1'), $('#cy2'), $('#cy3'), $('#cy4')),
+    new User("user4", NavPath_4, $('#cb1'), $('#cb2'), $('#cb3'), $('#cb4'))];
 
-function User(name, navPath) {
+function User(name, navPath, coinColor, place1, place2, place3, place4) {
     this.name = name;
     let score = 0;
     let path = navPath;
+    let color = coinColor;
+    let cPlacement = [];
+    cPlacement.push(place1, place2, place3, place4);
 
     this.getScore = function () {
         return score;
@@ -20,12 +23,26 @@ function User(name, navPath) {
     this.getPath = function () {
         return path;
     }
+
+    this.animateAvailableCoins = function () {
+
+    }
 }
 
-function getCurrentUser() {
+
+function GetCurrentUser() {
     let userItr = [
         users[0],
         users[1],
         users[2],
         users[3]];
+
+    this.getUser = function () {
+        return userItr[0];
+    }
+
+    this.switchUser = function () {
+        let tempUser = userItr.shift();
+        userItr.push(tempUser);
+    }
 }
